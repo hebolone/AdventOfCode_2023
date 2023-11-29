42dfd6633d0dcc108ef4e8235236161f2e78a1aa
Hebolone <polo.simone@gmail.com>
2023-11-29 22:22:17 +0100

    Solution!!!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿var daysBuilder = new DaysBuilder(basePath: @"C:\Users\simonep\source\repos\AoC\Aoc\Input\");
+﻿//var daysBuilder = new DaysBuilder(basePath: @"C:\Users\simonep\source\repos\AoC\Aoc\Input\");
+var daysBuilder = new DaysBuilder(basePath: @"/home/simone/Scrivania/AdventOfCode/2023/");
 
 daysBuilder
     .AddDay(1, new Day01())
